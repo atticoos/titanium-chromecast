0eab1fdd25fd8e2f25aa1edbe7f23dff05f64619
Atticus White <contact@atticuswhite.com>
2014-12-20 15:59:53 -0500

    GoogleCast framework, makefile

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
-all:
-	./install.sh
+all: clean build deploy
+
+build:
+	python iphone/build.py
+
+deploy:
+	unzip -o iphone/com.atticoos.titanium.chromecast-iphone-1.0.0.zip -d ~/Library/Application\ Support/Titanium/
+
+clean:
+	rm -rf iphone/build
---
+++ iphone/platform/GoogleCast.framework/Versions/A/Headers/GoogleCast.h
@@ -0,0 +1,27 @@
+// Cast SDK v2.5.1
+// Build 1396, generated on 2014-12-17 14:27.
+// Copyright 2013-2014 Google Inc.
+
+
+#import <GoogleCast/GCKApplicationMetadata.h>
+#import <GoogleCast/GCKCastChannel.h>
+#import <GoogleCast/GCKColor.h>
+#import <GoogleCast/GCKCommon.h>
+#import <GoogleCast/GCKDefines.h>
+#import <GoogleCast/GCKDevice.h>
+#import <GoogleCast/GCKDeviceManager.h>
+#import <GoogleCast/GCKDeviceScanner.h>
+#import <GoogleCast/GCKError.h>
+#import <GoogleCast/GCKFilterCriteria.h>
+#import <GoogleCast/GCKImage.h>
+#import <GoogleCast/GCKJSONUtils.h>
+#import <GoogleCast/GCKLaunchOptions.h>
+#import <GoogleCast/GCKLogger.h>
+#import <GoogleCast/GCKMediaControlChannel.h>
+#import <GoogleCast/GCKMediaInformation.h>
+#import <GoogleCast/GCKMediaMetadata.h>
+#import <GoogleCast/GCKMediaStatus.h>
+#import <GoogleCast/GCKMediaTextTrackStyle.h>
+#import <GoogleCast/GCKMediaTrack.h>
+#import <GoogleCast/GCKNSDictionary+TypedValueLookup.h>
+#import <GoogleCast/GCKSenderApplicationInfo.h>
